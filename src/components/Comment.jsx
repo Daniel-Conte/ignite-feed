85a3ecf354d5899cc602dcb950995520adc3fb4c
Daniel Conte <danielcontedev@gmail.com>
2023-01-26 19:35:40 -0300

    cria componente avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/Daniel-Conte.png" />
+      <Avatar hasBorder={false} src="https://github.com/Daniel-Conte.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from "phosphor-react";
 
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/Daniel-Conte.png"
-        />
+        <Avatar src="https://github.com/Daniel-Conte.png" />
         <strong>User</strong>
         <span>Web Developer</span>
       </div>
